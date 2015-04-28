925e0619475570c4a344db05e5a7dc81a52d6b95
santokis <santokis@onid.oregonstate.edu>
2015-04-27 22:25:11 -0700

    uploaded required files

---
+++ src/multtable.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<?php
+
+//This file should accept 4 parameters passed via the URL in a GET request.
+//min-multiplicand
+//max-multiplicand
+//min-multiplier
+//max-multiplier
+
+?>
+</body>
+</html>
