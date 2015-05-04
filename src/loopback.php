ca75c99f71e4897d58b0acb621b2b2d26c9f56a2
santokis <santokis@onid.oregonstate.edu>
2015-05-03 18:46:33 -0700

    updated loopback.php

--- src/loopback.php
+++ src/loopback.php
@@ -1,12 +1,27 @@
-<html>
-<head>
-<title></title>
-</head>
-<body>
 <?php
-
-
-
+    error_reporting(E_ALL);
+    ini_set('display', 1);
+    
+    //GET request
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        if(empty($_GET)){
+            //returns undefined if no value specified
+            echo '{"Type":"GET","parameters":null}';
+        }
+        else{
+            //print JSON object as string
+            echo '{"Type":"GET","parameters":' . json_encode($_GET) . '}';
+        }
+    }
+    //POST request
+    else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(empty($_POST)){
+            //returns undefined if no value specified
+            echo '{"Type":"POST","parameters":null}';
+        }
+        else{
+            //print JSON object as string
+            echo '{"Type":"POST","parameters":' . json_encode($_POST) . '}';
+        }
+    }
 ?>
-</body>
-</html>
